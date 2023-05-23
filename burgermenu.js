534fc189da150fc2aa7756a3f35d86bf583a2772
annafoverskov <119939347+annafoverskov@users.noreply.github.com>
2023-05-23 13:25:10 +0200

    .

---
+++ burgermenu.js
@@ -0,0 +1,8 @@
+function burgermenu() {
+    let x = document.getElementById("burgermenu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
