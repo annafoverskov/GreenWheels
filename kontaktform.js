e1c3a164f9f7f986d102303e88179c7ea0b31997
JohannaLanggaard <johanna.langgaard@gmail.com>
2023-05-29 08:41:02 +0200

    Update kontaktform.js

--- kontaktform.js
+++ kontaktform.js
@@ -4,22 +4,22 @@ select_element = document.getElementById("konSelect");
 let kon = ["Email", "Telefon opkald", "SMS besked"];
 
 
-// Udvider option-boks baseret på antal semestre i array'et
+// Udvider option-boks baseret på antal valgmuligheder i array'et
 for (let i=0; i < kon.length; i++) {
-let opt = kon[i];
-select_element.innerHTML += "<option value=\" "+ opt +"\">"+ opt + "</option";
+  let opt = kon[i];
+  select_element.innerHTML += "<option value=\" "+ opt +"\">"+ opt + "</option";
 }
 
 document.getElementById("konSelect").onchange = function(){
 if (document.getElementById("konSelect").value == 'Email')
 location.replace('index.html');
-else if (document.getElementById("konSelect").value == 'Telefon opkald')
-location.replace('index.html');
-else if (document.getElementById("konSelect").value == 'SMS besked')
-location.replace('index.html');
-else
-location.replace('index.html')
-}
+  else if (document.getElementById("konSelect").value == 'Telefon opkald')
+  location.replace('index.html');
+  else if (document.getElementById("konSelect").value == 'SMS besked')
+  location.replace('index.html');
+    else
+    location.replace('index.html')
+  }
 document.getElementById("konSelect").selectedIndex = 0;
 
 //kontaktform starter her
